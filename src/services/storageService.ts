09419e710f03b652cfd0dc981a33beeeb95fed69
devarogundade <devarogundade@gmail.com>
2025-10-05 09:28:15 +0100

    update

--- src/services/storageService.ts
+++ src/services/storageService.ts
@@ -15,13 +15,24 @@ const Storage = {
     onProgress?: (progress: number) => void
   ) {
     const firebaseConfig = {
-      apiKey: import.meta.env.VITE_FB_API_KEY_2,
-      authDomain: import.meta.env.VITE_FB_AUTH_DOMAIN_2,
-      projectId: import.meta.env.VITE_FB_PROJECT_ID_2,
-      storageBucket: import.meta.env.VITE_FB_STORAGE_BUCKET_2,
-      messagingSenderId: import.meta.env.VITE_FB_MESSAGING_SENDER_ID_2,
-      appId: import.meta.env.VITE_FB_APP_ID_2,
-      measurementId: import.meta.env.VITE_FB_MEASUREMENT_ID_2,
+      apiKey:
+        import.meta.env.VITE_FB_API_KEY_2 || import.meta.env.VITE_FB_API_KEY,
+      authDomain:
+        import.meta.env.VITE_FB_AUTH_DOMAIN_2 ||
+        import.meta.env.VITE_FB_AUTH_DOMAIN,
+      projectId:
+        import.meta.env.VITE_FB_PROJECT_ID_2 ||
+        import.meta.env.VITE_FB_PROJECT_ID,
+      storageBucket:
+        import.meta.env.VITE_FB_STORAGE_BUCKET_2 ||
+        import.meta.env.VITE_FB_STORAGE_BUCKET,
+      messagingSenderId:
+        import.meta.env.VITE_FB_MESSAGING_SENDER_ID_2 ||
+        import.meta.env.VITE_FB_MESSAGING_SENDER_ID,
+      appId: import.meta.env.VITE_FB_APP_ID_2 || import.meta.env.VITE_FB_APP_ID,
+      measurementId:
+        import.meta.env.VITE_FB_MEASUREMENT_ID_2 ||
+        import.meta.env.VITE_FB_MEASUREMENT_ID,
     };
 
     const app = initializeApp(firebaseConfig, "storage");
