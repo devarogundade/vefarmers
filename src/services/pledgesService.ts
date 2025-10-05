0a9b26dc88a1fdb7a9e26eb29eb5cefb247f562d
devarogundade <devarogundade@gmail.com>
2025-10-05 07:22:04 +0100

    update

--- src/services/pledgesService.ts
+++ src/services/pledgesService.ts
@@ -11,7 +11,6 @@ import {
   where,
   getDoc,
   updateDoc,
-  increment,
 } from "firebase/firestore";
 
 export class PledgesService {
@@ -68,32 +67,16 @@ export class PledgesService {
         };
       }
 
-      const newPledge: Pledge = {
-        id: `${farmerResponse.data.address}-${pledgerAddress}`,
-        pledgerAddress,
-        farmerAddress: pledgeData.farmerAddress,
-        farmer: {
-          name: farmerResponse.data.name,
-          address: farmerResponse.data.address,
-          pledgeManager: farmerResponse.data.pledgeManager,
-          preferredPool: farmerResponse.data.preferredPool,
-          location: farmerResponse.data.location,
-          farmSize: farmerResponse.data.farmSize,
-          cropType: farmerResponse.data.cropType,
-          verified: false,
-        },
-        amount: pledgeData.amount,
-        currency: pledgeData.currency,
-        createdAt: new Date().toISOString(),
-      };
+      const id = `${farmerResponse.data.address}-${pledgerAddress}`;
 
       const db = getFirestore();
 
-      const docSnap = await getDoc(doc(db, "pledges", newPledge.id));
+      const docSnap = await getDoc(doc(db, "pledges", id));
 
       if (docSnap.exists()) {
-        await updateDoc(doc(db, "pledges", newPledge.id), {
-          amount: increment(newPledge.amount),
+        const data = docSnap.data() as Pledge;
+        await updateDoc(doc(db, "pledges", id), {
+          amount: data.amount + pledgeData.amount,
           farmer: {
             name: farmerResponse.data.name,
             address: farmerResponse.data.address,
@@ -105,17 +88,41 @@ export class PledgesService {
             verified: false,
           },
         });
+
+        return {
+          data: { ...data, amount: data.amount + pledgeData.amount },
+          success: true,
+          message: "Pledge updated successfully",
+        };
       } else {
+        const newPledge: Pledge = {
+          id,
+          pledgerAddress,
+          farmerAddress: pledgeData.farmerAddress,
+          farmer: {
+            name: farmerResponse.data.name,
+            address: farmerResponse.data.address,
+            pledgeManager: farmerResponse.data.pledgeManager,
+            preferredPool: farmerResponse.data.preferredPool,
+            location: farmerResponse.data.location,
+            farmSize: farmerResponse.data.farmSize,
+            cropType: farmerResponse.data.cropType,
+            verified: false,
+          },
+          amount: pledgeData.amount,
+          currency: pledgeData.currency,
+          createdAt: new Date().toISOString(),
+        };
         await setDoc(doc(db, "pledges", newPledge.id), newPledge, {
           merge: true,
         });
-      }
 
-      return {
-        data: newPledge,
-        success: true,
-        message: "Pledge created successfully",
-      };
+        return {
+          data: newPledge,
+          success: true,
+          message: "Pledge created successfully",
+        };
+      }
     } catch (error) {
       throw new Error("Failed to create pledge");
     }
@@ -147,8 +154,9 @@ export class PledgesService {
       const docSnap = await getDoc(doc(db, "pledges", pledge.id));
 
       if (docSnap.exists()) {
+        const data = docSnap.data() as Pledge;
         await updateDoc(doc(db, "pledges", pledge.id), {
-          amount: increment(-1 * pledge.amount),
+          amount: data.amount - pledge.amount,
           farmer: {
             name: farmerResponse.data.name,
             address: farmerResponse.data.address,
